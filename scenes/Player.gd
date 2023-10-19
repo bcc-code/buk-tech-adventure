eedff94852036ee0976a1f890b3ca4ca63f8f3bc
Ghostbird <gijsbertth@gmail.com>
2023-10-19 21:02:27 +0200

    Add angular offset between input and movement

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -4,9 +4,11 @@ extends CharacterBody2D
 @export var player_speed = 234
 ## Friction is per frame. You may slide weirdly on very low FPS
 @export var friction = 0.75
+## Difference in direction (degrees) between input direction and movement direction.
+@export var input_offset_angle = 45
 
 func _physics_process(delta):
-	var motion = Input.get_vector("move_west", "move_east", "move_north", "move_south")
+	var motion = Input.get_vector("move_west", "move_east", "move_north", "move_south").rotated(deg_to_rad(input_offset_angle))
 	# The isometric tiles y/x aspect is 1/2, so we must scale the motion by that factor
 	motion.y /= 2
 	# No delta factor required, move_and_slide already does that!
